5a730d9bb8743ca298d5821096dddfd1d5763b4c
jaranchai <jaranchai.nt@hotmail.com>
2024-02-13 12:33:35 +0700

    [Added] error helper handle

---
+++ store-web/src/utils/helper.ts
@@ -0,0 +1,27 @@
+import { AxiosError, isAxiosError } from 'axios'
+
+export const handleServiceError = (error: AxiosError | unknown) => {
+  let response = null
+  if (isAxiosError(error)) {
+    response = handleAxiosError(error)
+  } else {
+    response = handleUnexpectedError(error)
+  }
+
+  return response
+}
+
+export const handleAxiosError = (error: AxiosError) => {
+  return {
+    status: 'error',
+    message: error.message
+  }
+}
+
+export const handleUnexpectedError = (error: unknown) => {
+  console.log('Service Error: ', error)
+  return {
+    status: 'error',
+    message: 'Unknown Error'
+  }
+}
