21c23c69c14416c157939cbd383929f582e6a7cd
jaranchai <jaranchai.nt@hotmail.com>
2024-02-13 17:02:23 +0700

    [Edited] add id on product cart

--- store-web/src/app/product/list/components/product-card.tsx
+++ store-web/src/app/product/list/components/product-card.tsx
@@ -15,9 +15,10 @@ type ProductCardProps = {
 
 const ProductCard = ({ data }: ProductCardProps) => {
   return (
-    <div className="group relative">
+    <div id={`product-card-${data.id}`} className="group relative">
       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
         <Image
+          id={`product-card-image-${data.id}`}
           src={`${config.imageUrl}${data.product_image}`}
           alt={data.product_name}
           width={280}
@@ -26,13 +27,17 @@ const ProductCard = ({ data }: ProductCardProps) => {
         />
       </div>
       <div className="mt-4 mb-1 flex justify-between">
-        <Header4 className="text-gray-700">
+        <Header4 id={`product-card-name-${data.id}`} className="text-gray-700">
           <a href={`/product/${data.id}`}>
             <span aria-hidden="true" className="absolute inset-0" />
             {data.product_name}
           </a>
         </Header4>
-        <Text size="md" className="font-medium text-gray-900">
+        <Text
+          id={`product-card-price-${data.id}`}
+          size="md"
+          className="font-medium text-gray-900"
+        >
           {convertCurrency(data.product_price, 'THB')}
         </Text>
       </div>
