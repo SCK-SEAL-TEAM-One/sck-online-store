535d1cd14bfaa278f19c023ca0b47720055fb56a
jaranchai <jaranchai.nt@hotmail.com>
2024-02-15 10:35:04 +0700

    [Edited] change use props on input field

--- store-web/src/components/input-field/index.tsx
+++ store-web/src/components/input-field/index.tsx
@@ -5,25 +5,60 @@
 type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
   label?: string
   id?: string
+  type: string
+  name?: string
+  placeholder?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onFocus?: (e: React.FocusEvent<HTMLInputElement>) => void
+  required?: boolean
+  maxLength?: number
+  value?: string
+  readOnly?: boolean
+  disabled?: boolean
 }
 
-const InputField = (props: InputFieldProps) => {
+const InputField = ({
+  label,
+  id,
+  type,
+  name,
+  placeholder,
+  onChange,
+  onFocus,
+  required,
+  maxLength,
+  value,
+  readOnly,
+  disabled
+}: InputFieldProps) => {
   return (
-    <div className={props.label ? 'mb-2' : ''}>
-      {props.label ? (
-        <label
-          htmlFor={props.id}
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          {props.label}
-        </label>
-      ) : null}
+    <>
+      <div className={label ? 'mb-2' : ''}>
+        {label ? (
+          <label
+            htmlFor={`${id}-input`}
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            {label}
+          </label>
+        ) : null}
 
-      <input
-        className="bg-white text-sm w-full px-3 py-2 text-gray-900 border border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
-        {...props}
-      />
-    </div>
+        <input
+          className="bg-white text-sm w-full px-3 py-2 text-gray-900 border border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
+          id={`${id}-input`}
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          onChange={onChange}
+          onFocus={onFocus}
+          required={required}
+          maxLength={maxLength}
+          value={value}
+          readOnly={readOnly}
+          disabled={disabled}
+        />
+      </div>
+    </>
   )
 }
 
