3707001c695630c3639a4be1e3249b2701f0ba40
jaranchai <jaranchai.nt@hotmail.com>
2024-02-15 09:30:53 +0700

    [Edited] change input props and fix bug zero value

--- store-web/src/app/cart/components/product-item.tsx
+++ store-web/src/app/cart/components/product-item.tsx
@@ -45,7 +45,6 @@ const ProductItem = ({
         updateQuantity(value)
       } else {
         setNewQuantity(quantity)
-        updateQuantity(quantity)
       }
     }
   }
--- store-web/src/components/input-quantity/index.tsx
+++ store-web/src/components/input-quantity/index.tsx
@@ -8,44 +8,65 @@ type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
   label?: string
   id?: string
   isHiddenLable?: boolean
+  placeholder?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onBlur?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  value?: string | number
+  required?: boolean
   decrement: () => void
   increment: () => void
 }
 
-const InputQuantity = (props: InputFieldProps) => {
+const InputQuantity = ({
+  id,
+  label,
+  placeholder,
+  increment,
+  decrement,
+  onChange,
+  onBlur,
+  value,
+  isHiddenLable,
+  required
+}: InputFieldProps) => {
   return (
     <div>
-      {props.label && !props.isHiddenLable ? (
+      {label && !isHiddenLable ? (
         <label
-          id={`${props.id}-label`}
-          htmlFor={`${props.id}-input`}
+          id={`${id}-label`}
+          htmlFor={`${id}-input`}
           className="block mb-2 text-sm font-medium text-gray-900"
         >
-          {props.label}
+          {label}
         </label>
       ) : null}
 
       <div className="relative flex items-center max-w-[10rem]">
         <button
           type="button"
-          id={`${props.id}-decrement-btn`}
+          id={`${id}-decrement-btn`}
           data-input-counter-decrement="quantity-input"
           className="bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 focus:ring-2 focus:outline-none"
-          onClick={props.decrement}
+          onClick={decrement}
         >
           <MinusIcon className="w-3 h-3 text-gray-900" />
         </button>
         <input
           type="text"
           className="bg-gray-100 border border-gray-300 border-x-0 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5"
-          {...props}
+          id={`${id}-input`}
+          onChange={onChange}
+          onBlur={onBlur}
+          placeholder={placeholder}
+          value={value}
+          required={required}
         />
         <button
           type="button"
-          id={`${props.id}-increment-btn`}
+          id={`${id}-increment-btn`}
           data-input-counter-increment="quantity-input"
           className="bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100"
-          onClick={props.increment}
+          onClick={increment}
         >
           <PlusIcon className="w-3 h-3 text-gray-900" />
         </button>
