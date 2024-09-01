7350ada2dd14b3a153b753de6328681b970b1c36
srankmeng <jaturong.duang@gmail.com>
2024-09-01 20:13:53 +0700

    [Updated] more eslint rules for frontend

--- store-web/src/utils/credit-cart-format.ts
+++ store-web/src/utils/credit-cart-format.ts
@@ -16,24 +16,24 @@ export const formatCreditCardNumber = (value: string) => {
   let nextValue
 
   switch (issuer) {
-    case 'amex':
-      nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
-        4,
-        10
-      )} ${clearValue.slice(10, 15)}`
-      break
-    case 'dinersclub':
-      nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
-        4,
-        10
-      )} ${clearValue.slice(10, 14)}`
-      break
-    default:
-      nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
-        4,
-        8
-      )} ${clearValue.slice(8, 12)} ${clearValue.slice(12, 19)}`
-      break
+  case 'amex':
+    nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
+      4,
+      10
+    )} ${clearValue.slice(10, 15)}`
+    break
+  case 'dinersclub':
+    nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
+      4,
+      10
+    )} ${clearValue.slice(10, 14)}`
+    break
+  default:
+    nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
+      4,
+      8
+    )} ${clearValue.slice(8, 12)} ${clearValue.slice(12, 19)}`
+    break
   }
 
   return nextValue.trim()
