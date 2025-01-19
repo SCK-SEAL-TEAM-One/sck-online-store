501f68835d565c2c7ceb8cfc9eb79a3738fecaee
karan sivarat <bomb0069@gmail.com>
2025-01-19 15:12:03 +0700

    [Edited]: change sequence of point test and add case of 100

--- store-service/internal/common/point_test.go
+++ store-service/internal/common/point_test.go
@@ -7,26 +7,39 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_CalculatePoint_Input_599_Point_999_Should_be_Return_5(t *testing.T) {
+func Test_CalculatePoint_Input_0_Should_be_Return_0(t *testing.T) {
+	expected := 0
+	price := 0.0
+
+	actual := common.CalculatePoint(price)
+
+	assert.Equal(t, expected, actual)
+}
+
+func Test_CalculatePoint_Input_100_Should_be_Return_1(t *testing.T) {
+	expected := 1
+	price := 100.0
+
+	actual := common.CalculatePoint(price)
+
+	assert.Equal(t, expected, actual)
+}
+
+func Test_CalculatePoint_Input_599_Dot_999_Should_be_Return_5(t *testing.T) {
 	expected := 5
+	price := 599.999
 
-	actual := common.CalculatePoint(599.999)
+	actual := common.CalculatePoint(price)
 
 	assert.Equal(t, expected, actual)
 }
 
-func Test_CalculatePoint_Input_Minus_599_Point_999_Should_be_Return_0(t *testing.T) {
+func Test_CalculatePoint_Input_Minus_599_Dot_999_Should_be_Return_0(t *testing.T) {
 	expected := 0
+	price := -599.999
 
-	actual := common.CalculatePoint(-599.999)
-
-	assert.Equal(t, expected, actual)
-}
-
-func Test_CalculatePoint_Input_0_Point_999_Should_be_Return_0(t *testing.T) {
-	expected := 0
-
-	actual := common.CalculatePoint(0)
+	actual := common.CalculatePoint(price)
 
 	assert.Equal(t, expected, actual)
 }
+
