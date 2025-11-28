552bcdd616120b59eb0b66aee3ef2dfe57e6e7d7
klimprakhon <klimprakhon@gmail.com>
2025-11-28 11:03:45 +0700

    [Added]: Axios Response Interceptors in case of 401

--- store-web/src/utils/axios.ts
+++ store-web/src/utils/axios.ts
@@ -25,4 +25,16 @@ axiosShoppingMallApi.interceptors.request.use(
   }
 )
 
+axiosShoppingMallApi.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      window.location.href = '/auth/login'
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default axiosShoppingMallApi
