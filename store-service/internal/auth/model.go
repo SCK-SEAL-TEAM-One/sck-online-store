e70e02111363374bdfafbd2f384d8cc8cb5267b1
klimprakhon <klimprakhon@gmail.com>
2025-12-02 17:17:55 +0700

    [Edited]: Add User, UserPayload and toPayload to adapt user info to token payload

--- store-service/internal/auth/model.go
+++ store-service/internal/auth/model.go
@@ -4,10 +4,34 @@ import (
 	jwt "github.com/golang-jwt/jwt/v5"
 )
 
+type User struct {
+	ID        int    `json:"id" db:"id"`
+	Username  string `json:"username" db:"username"`
+	FirstName string `json:"first_name" db:"first_name"`
+	LastName  string `json:"last_name" db:"last_name"`
+	Password  string `json:"password" db:"password"`
+}
+
+type UserPayload struct {
+	UserID    int    `json:"user_id" db:"id"`
+	FirstName string `json:"first_name" db:"first_name"`
+	LastName  string `json:"last_name" db:"last_name"`
+	Username  string `json:"username" db:"username"`
+}
+
 type Claims struct {
 	UserID    int    `json:"user_id" db:"id"`
 	FirstName string `json:"first_name" db:"first_name"`
 	LastName  string `json:"last_name" db:"last_name"`
 	Username  string `json:"username" db:"username"`
 	jwt.RegisteredClaims
+}
+
+func (claim Claims) ToPayload() UserPayload {
+	return UserPayload{
+		UserID:    claim.UserID,
+		FirstName: claim.FirstName,
+		LastName:  claim.LastName,
+		Username:  claim.Username,
+	}
 }
