e012050ccd3e48bf2517cdd150507aeab8e6107b
klimprakhon <klimprakhon@gmail.com>
2025-12-04 16:26:16 +0700

    [Edited]: conditional render if there is authUser in the Header's right menu

--- store-web/src/layouts/common/components/right-menu.tsx
+++ store-web/src/layouts/common/components/right-menu.tsx
@@ -1,16 +1,25 @@
 'use client'
 
+import { useUserStore } from '@/hooks/use-user-store'
 import Cart from '@/layouts/common/components/cart'
 import Login from '@/layouts/common/components/login'
 import { HeaderProps } from '@/layouts/common/header'
-
+import { UserCircleIcon } from '@heroicons/react/16/solid'
 // ---------------------------------------------------
 
 const RightMenu = ({ setShoppingCartOpen }: HeaderProps) => {
+  const user = useUserStore((state) => state.user)
   return (
-    <div className="flex flex-1 gap-x-12 justify-end">
+    <div className="flex flex-1 gap-x-10 justify-end">
       <Cart setShoppingCartOpen={setShoppingCartOpen} />
-      <Login />
+      {user ? (
+        <div className="flex justify-center items-center gap-1">
+          <UserCircleIcon className="h-7 w-7 text-gray-800" />
+          {/* <span>{user.firstName.toLocaleUpperCase()}</span> */}
+        </div>
+      ) : (
+        <Login />
+      )}
     </div>
   )
 }
