bbb712224c69b8be917d205b1b803f39c2e5300d
klimprakhon <klimprakhon@gmail.com>
2025-12-08 09:41:37 +0700

    [Added]: checking AccessToken before entering to the general pages

--- store-web/src/layouts/common/index.tsx
+++ store-web/src/layouts/common/index.tsx
@@ -1,11 +1,35 @@
 import ShoppingCartView from '@/app/cart/shopping-cart'
+import { UserInfo, useUserStore } from '@/hooks/use-user-store'
 import Header from '@/layouts/common/header'
-import { useState } from 'react'
+import { decodeJWT } from '@/utils/jwt'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 // ----------------------------------------------------------------------------
 
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
   const [isShoppingCartOpen, setShoppingCartOpen] = useState(false)
+  const { user, setUser } = useUserStore()
+  const accessToken = localStorage.getItem('accessToken')
+  const route = useRouter()
+
+  useEffect(() => {
+    if (!accessToken) {
+      route.push('/auth/login')
+      return
+    }
+
+    if (!user) {
+      const payload = decodeJWT(accessToken)
+      const user: UserInfo = {
+        userId: payload.user_id,
+        firstName: payload.first_name,
+        lastName: payload.last_name,
+        username: payload.username
+      }
+      setUser(user)
+    }
+  }, [user, accessToken, route, setUser])
 
   return (
     <>
