c8d61e88219c01dca429630724cb3ccb3eeec36a
klimprakhon <klimprakhon@gmail.com>
2025-12-26 11:54:44 +0700

    [Edited]: add style for Login page.tsx for spacing

--- store-web/src/app/auth/login/page.tsx
+++ store-web/src/app/auth/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from './components/login-form'
 
 const LoginPage = () => {
   return (
-    <div id="login-page">
+    <div id="login-page" className="w-full flex items-center justify-center">
       <LoginForm />
     </div>
   )
