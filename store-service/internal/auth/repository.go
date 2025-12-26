5605cb4df2e37832941251fc64f06d3cdea881eb
klimprakhon <klimprakhon@gmail.com>
2025-12-26 11:42:52 +0700

    [Edited]: update data type for UserRepository.FindByID not to get the passsword from DB

--- store-service/internal/auth/repository.go
+++ store-service/internal/auth/repository.go
@@ -6,7 +6,7 @@ import (
 
 type UserRepository interface {
 	FindByUsername(username string) (User, error)
-	FindByID(uid int) (User, error)
+	FindByID(uid int) (UserPayload, error)
 }
 
 type UserRepositoryMySQL struct {
@@ -20,9 +20,9 @@ func (repository UserRepositoryMySQL) FindByUsername(username string) (User, err
 	return user, err
 }
 
-func (repository UserRepositoryMySQL) FindByID(uid int) (User, error) {
-	var user User
-	query := `SELECT id, username, first_name, last_name, password FROM users WHERE id = ?`
+func (repository UserRepositoryMySQL) FindByID(uid int) (UserPayload, error) {
+	var user UserPayload
+	query := `SELECT id, username, first_name, last_name FROM users WHERE id = ?`
 	err := repository.DBConnection.Get(&user, query, uid)
 	return user, err
 }
